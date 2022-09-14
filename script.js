9002e5176caad6cefa47a2db4861d453518b2ca9
Rogemateos <rogemateos@hotmail.com>
2022-09-14 23:47:12 +0100

    Adding logig if input is empty pass two parameters,username and message, and create a function showError to add class and message

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+//Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+//Event Listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (username.value === "") {
-    alert("username is required");
+    showError(username, "username is required");
+  } else {
+    showSuccess(username);
   }
 });
