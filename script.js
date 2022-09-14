8ecf1b5e54092a28133adb6297b1d1a22e184365
Rogemateos <rogemateos@hotmail.com>
2022-09-15 00:23:54 +0100

    Adding email  regular expression

--- script.js
+++ script.js
@@ -17,6 +17,17 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
+// Check email is valid
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
 //Event Listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
