93432810d846ee7faf92048aca85a6f9c5054583
Rogemateos <rogemateos@hotmail.com>
2022-09-14 23:20:39 +0100

    Checking empty field in input

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ const password2 = document.getElementById("password2");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(username.value);
+  if (username.value === "") {
+    alert("username is required");
+  }
 });
